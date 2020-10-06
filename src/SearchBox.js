2eb610d9d795bec0dc4c3a6f4be254fe38c5aaa4
amanbijalwan <36789793+amanbijalwan@users.noreply.github.com>
2020-10-06 15:06:29 +0530

    Changes >> search bar(not functional currently)
    Next version will be modified with separate class for searchbar. Updates with separate js script for SearchBox and linked it with Appjs

---
+++ src/SearchBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return(
+        <div className="pa2">
+            <input type="search" placeholder="Find Robot" className="pa3 ba b--green bg-lightest-blue" />
+        </div>
+        
+    );
+}
+
+export default SearchBox;
