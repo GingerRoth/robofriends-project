64bc4ffd6163f2a705f5960c702bfd4169cce4e4
amanbijalwan <36789793+amanbijalwan@users.noreply.github.com>
2020-10-11 01:55:54 +0530

    Changes/Updates with Build
    --Main script is modified with api for external additions of robos
    --Robos changed and updated in unique way
    --Other updates and changes in constructors and others
    --Launch ready build for static servers

---
+++ build/precache-manifest.24c1ceadfc83fb3fb7d8e7b7660606e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642a8ec916278195d886b7aed9bd2f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "597cb5d05eb5f3c49a76",
+    "url": "/static/css/main.b311d25b.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597cb5d05eb5f3c49a76",
+    "url": "/static/js/main.820a4a71.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React,{Component} from 'react';
+import CardList from '../components/CardList';
+import { robots } from '../robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css'
+
+class App extends Component {
+    constructor() {
+        super()
+            this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            return response.json();
+        })
+        .then(users => {
+            this.setState({robots: robots})
+        })
+
+    }
+
+    OnSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        //console.log(event.target.value);
+        //console.log(FilterRobots)
+    }
+
+    render() {
+        const { robots, searchfield} = this.state;
+
+        const FilterRobots = robots.filter(robots => {
+        return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        if(robots.length === 0){
+            return <h1 className="tc">Loading Robos, Please wait!</h1>
+        } else {
+            return(
+                <div className="tc">
+                    <h1 className='f1'>Cyber Buddies</h1>
+
+                    <SearchBox SearchChange={this.OnSearchChange}/>
+                    <Scroll>
+                        <CardList robots={ FilterRobots }/>
+                    </Scroll>
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
